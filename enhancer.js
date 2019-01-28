fbd05c76d2ae225127963c9dc49953b9a480a8d1
steelworx <eileencuevas62@gmail.com>
2019-01-28 18:30:35 -0500

    added fail method to enhancer.js

--- enhancer.js
+++ enhancer.js
@@ -4,14 +4,38 @@ module.exports = {
     repair
 }
 
-const displayName = [];
+const displayName = [
+    '', '[+1]', '[+2]', 
+    '[+3]', '[+4]', '[+5]', 
+    '[+6]', '[+7]', '[+8]', 
+    '[+9]', '[+10]', '[+11]', 
+    '[+12]', '[+13]', '[+14]', 
+    '[+15]', '[PRI]', '[DUO]', 
+    '[TRI]', '[TET]', '[PEN]',
+];
 
 function success(item) {
     // take an item, changes the name and increases enhancement
 }
 
 function fail(item) {
-    //
+    const result = { ...item }
+    // take an item, decreases durability & enhancement (if level > 16), changes name
+    if ( item.enhancement <= 14) {
+        return { ...item, durability: item.durability - 5, };
+    } else if (item.enhancement <= 16) {
+        return {
+            ...item, durability: item.durability - 10,};
+    } else if (item.enhancement <= 19) {
+        return {
+            ...item,
+            durability: item.durability - 10,
+            enhancement: item.enhancement - 1,
+            currentName: `${displayName[item.enhancement -1]} ${item.originalName}`
+        };
+    } else {
+        return 'Nothing changed.';
+    }
 }
 
 function repair(item) {
